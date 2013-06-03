d84ae3e3990e1b5eeef4adbb7bd1ffeff1161e0b
Michael Nitschinger <michael@nitschinger.at>
2013-06-03 12:44:07 +0200

    DATACOUCH-4 - Add Apache2 License Headers and @author field.

--- src/main/java/org/springframework/data/couchbase/repository/config/EnableCouchbaseRepositories.java
+++ src/main/java/org/springframework/data/couchbase/repository/config/EnableCouchbaseRepositories.java
@@ -1,5 +1,20 @@
-package org.springframework.data.couchbase.repository.config;
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+package org.springframework.data.couchbase.repository.config;
 
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ComponentScan.Filter;
@@ -15,6 +30,8 @@ import java.lang.annotation.Target;
 /**
  * Annotation to activate Couchbase repositories. If no base package is configured through either {@link #value()},
  * {@link #basePackages()} or {@link #basePackageClasses()} it will trigger scanning of the package of annotated class.
+ *
+ * @author Michael Nitschinger
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
--- src/main/java/org/springframework/data/couchbase/repository/support/CouchbaseRepositoryFactoryBean.java
+++ src/main/java/org/springframework/data/couchbase/repository/support/CouchbaseRepositoryFactoryBean.java
@@ -1,5 +1,20 @@
-package org.springframework.data.couchbase.repository.support;
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+package org.springframework.data.couchbase.repository.support;
 
 import org.springframework.data.couchbase.core.CouchbaseOperations;
 import org.springframework.data.repository.Repository;
@@ -9,6 +24,9 @@ import org.springframework.util.Assert;
 
 import java.io.Serializable;
 
+/**
+ * @author Michael Nitschinger
+ */
 public class CouchbaseRepositoryFactoryBean<T extends Repository<S, ID>, S, ID extends Serializable> extends
   RepositoryFactoryBeanSupport<T, S, ID> {
 
