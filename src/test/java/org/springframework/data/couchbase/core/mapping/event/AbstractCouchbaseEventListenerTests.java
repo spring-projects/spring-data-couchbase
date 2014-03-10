ea6b14f4c99d7800c10b21ce160d30f8886d0f1e
Michael Nitschinger <michael@nitschinger.at>
2014-03-10 14:08:49 +0100

    DATACOUCH-71 - add execution listener to test

--- src/test/java/org/springframework/data/couchbase/core/mapping/event/AbstractCouchbaseEventListenerTests.java
+++ src/test/java/org/springframework/data/couchbase/core/mapping/event/AbstractCouchbaseEventListenerTests.java
@@ -21,7 +21,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.couchbase.core.CouchbaseTemplate;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
 import static org.junit.Assert.assertEquals;
 
@@ -30,6 +32,7 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = EventContextConfiguration.class)
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class})
 public class AbstractCouchbaseEventListenerTests {
 
   @Autowired
