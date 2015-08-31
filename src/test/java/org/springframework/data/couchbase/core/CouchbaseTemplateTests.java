6fec4882ef3041b001d4acfad8f45c80c8fcb4aa
Oliver Gierke <info@olivergierke.de>
2015-08-31 18:20:58 +0200

    DATACOUCH-159 - Polishing.
    Extended copyright years in touched files. Added author tags and reference to ticket from the test case.
    
    Original pull requests: #67.

--- src/test/java/org/springframework/data/couchbase/core/CouchbaseTemplateTests.java
+++ src/test/java/org/springframework/data/couchbase/core/CouchbaseTemplateTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 the original author or authors.
+ * Copyright 2013-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,6 +43,7 @@ import static org.junit.Assert.*;
 
 /**
  * @author Michael Nitschinger
+ * @author Anastasiia Smirnova
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestApplicationConfig.class)
@@ -188,6 +189,9 @@ public class CouchbaseTemplateTests {
     }
   }
 
+  /**
+   * @see DATACOUCH-159
+   */
   @Test
   public void shouldDeserialiseLongsAndInts() {
     final long longValue = new Date().getTime();
