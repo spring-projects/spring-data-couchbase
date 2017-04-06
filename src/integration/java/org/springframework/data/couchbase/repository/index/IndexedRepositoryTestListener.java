4b78715ed58e97ac9affcdfe3012f3fbb49e53c1
Subhashni Balakrishnan <b.subhashni@gmail.com>
2017-04-06 15:57:30 -0700

    DATACOUCH-290 - Upgrade to java-client 2.4.4
    Original PR: #138.

--- src/integration/java/org/springframework/data/couchbase/repository/index/IndexedRepositoryTestListener.java
+++ src/integration/java/org/springframework/data/couchbase/repository/index/IndexedRepositoryTestListener.java
@@ -1,6 +1,7 @@
 package org.springframework.data.couchbase.repository.index;
 
 import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.error.DesignDocumentDoesNotExistException;
 import com.couchbase.client.java.query.Index;
 import com.couchbase.client.java.query.N1qlQuery;
 
@@ -12,14 +13,19 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
  * A test listener that will remove the indexes created in {@link IndexedRepositoryTests} before test case is run.
  *
  * @author Simon Baslé
+ * @author Subhashni Balakrishnan
  */
 public class IndexedRepositoryTestListener extends DependencyInjectionTestExecutionListener {
 
   @Override
   public void beforeTestClass(final TestContext testContext) throws Exception {
     Bucket client = (Bucket) testContext.getApplicationContext().getBean(BeanNames.COUCHBASE_BUCKET);
-    client.bucketManager().removeDesignDocument(IndexedRepositoryTests.VIEW_DOC);
-    client.bucketManager().removeDesignDocument("foo");
+    try {
+      client.bucketManager().removeDesignDocument(IndexedRepositoryTests.VIEW_DOC);
+      client.bucketManager().removeDesignDocument("foo");
+    } catch (DesignDocumentDoesNotExistException ex) {
+      //ignore
+    }
     client.query(N1qlQuery.simple(Index.dropPrimaryIndex(client.name())));
     client.query(N1qlQuery.simple(Index.dropIndex(client.name(), IndexedRepositoryTests.SECONDARY)));
   }
