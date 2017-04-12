611657d65d0729cc9ad464e0daaa006eb31b6ce8
Subhashni Balakrishnan <b.subhashni@gmail.com>
2017-04-12 11:10:30 -0700

    DATACOUCH-275 Key auto generation using attributes, uuid
    Original pull request: #142.

---
+++ src/integration/java/org/springframework/data/couchbase/IntegrationTestCustomKeySettings.java
@@ -0,0 +1,7 @@
+package org.springframework.data.couchbase;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class IntegrationTestCustomKeySettings extends IntegrationTestApplicationConfig {
+}
