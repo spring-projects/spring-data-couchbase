e8c647fb9de793091877fedc5349f31cc8c7af6c
Mark Paluch <mpaluch@pivotal.io>
2017-11-25 11:06:59 +0100

    DATACOUCH-350 - Polishing.
    Add test to verify configuration extension. Fix reactive module name to match non-reactive module name. Add author tags. Reorder methods, add javadoc comments.
    
    Original pull request: #153.

--- src/main/java/org/springframework/data/couchbase/repository/config/ReactiveCouchbaseRepositoryConfigurationExtension.java
+++ src/main/java/org/springframework/data/couchbase/repository/config/ReactiveCouchbaseRepositoryConfigurationExtension.java
@@ -19,22 +19,22 @@ import java.lang.annotation.Annotation;
 import java.util.Collection;
 import java.util.Collections;
 
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.data.config.ParsingUtils;
+import org.springframework.data.couchbase.config.BeanNames;
 import org.springframework.data.couchbase.core.mapping.Document;
 import org.springframework.data.couchbase.repository.ReactiveCouchbaseRepository;
 import org.springframework.data.couchbase.repository.support.ReactiveCouchbaseRepositoryFactoryBean;
+import org.springframework.data.repository.config.AnnotationRepositoryConfigurationSource;
 import org.springframework.data.repository.config.RepositoryConfigurationExtensionSupport;
 import org.springframework.data.repository.config.XmlRepositoryConfigurationSource;
 import org.springframework.data.repository.core.RepositoryMetadata;
 import org.w3c.dom.Element;
 
-import org.springframework.beans.factory.support.BeanDefinitionBuilder;
-import org.springframework.data.config.ParsingUtils;
-import org.springframework.data.couchbase.config.BeanNames;
-import org.springframework.data.repository.config.AnnotationRepositoryConfigurationSource;
-
 /**
  * @author Subhashni Balakrishnan
  * @author Mark Paluch
+ * @author Alexander Derkach
  * @since 3.0
  */
 public class ReactiveCouchbaseRepositoryConfigurationExtension extends RepositoryConfigurationExtensionSupport {
@@ -45,48 +45,80 @@ public class ReactiveCouchbaseRepositoryConfigurationExtension extends Repositor
 	/** The reference property to use in xml configuration to specify the index manager bean to use with a repository. */
 	private static final String COUCHBASE_INDEX_MANAGER_REF = "couchbase-index-manager-ref";
 
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.repository.config.RepositoryConfigurationExtensionSupport#getModuleName()
+	 */
 	@Override
-	protected String getModulePrefix() {
-		return "reactive-couchbase";
-	}
-
-	public String getRepositoryFactoryBeanClassName() {
-		return ReactiveCouchbaseRepositoryFactoryBean.class.getName();
-	}
-
-	@Override
-	public void postProcess(final BeanDefinitionBuilder builder, final XmlRepositoryConfigurationSource config) {
-		Element element = config.getElement();
-		ParsingUtils.setPropertyReference(builder, element, REACTIVE_COUCHBASE_TEMPLATE_REF, "reactiveCouchbaseOperations");
-		ParsingUtils.setPropertyReference(builder, element, COUCHBASE_INDEX_MANAGER_REF, "indexManager");
-	}
-
-	@Override
-	public void postProcess(final BeanDefinitionBuilder builder, final AnnotationRepositoryConfigurationSource config) {
-		builder.addDependsOn(BeanNames.REACTIVE_COUCHBASE_OPERATIONS_MAPPING);
-		builder.addDependsOn(BeanNames.COUCHBASE_INDEX_MANAGER);
-		builder.addPropertyReference("couchbaseOperationsMapping", BeanNames.REACTIVE_COUCHBASE_OPERATIONS_MAPPING);
-		builder.addPropertyReference("indexManager", BeanNames.COUCHBASE_INDEX_MANAGER);
+	public String getModuleName() {
+		return "Couchbase";
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.springframework.data.repository.config.RepositoryConfigurationExtensionSupport#getIdentifyingAnnotations()
+	 * @see org.springframework.data.repository.config.RepositoryConfigurationExtensionSupport#getModulePrefix()
 	 */
+	@Override
+	protected String getModulePrefix() {
+		return "couchbase";
+	}
+
+	/*
+	* (non-Javadoc)
+	* @see org.springframework.data.repository.config.RepositoryConfigurationExtension#getRepositoryFactoryBeanClassName()
+	*/
+	public String getRepositoryFactoryBeanClassName() {
+		return ReactiveCouchbaseRepositoryFactoryBean.class.getName();
+	}
+
+	/*
+	* (non-Javadoc)
+	* @see org.springframework.data.repository.config.RepositoryConfigurationExtensionSupport#getIdentifyingAnnotations()
+	*/
 	@Override
 	protected Collection<Class<? extends Annotation>> getIdentifyingAnnotations() {
 		return Collections.singleton(Document.class);
 	}
 
 	/*
-     * (non-Javadoc)
-     * @see org.springframework.data.repository.config.RepositoryConfigurationExtensionSupport#getIdentifyingTypes()
-     */
+	 * (non-Javadoc)
+	 * @see org.springframework.data.repository.config.RepositoryConfigurationExtensionSupport#getIdentifyingTypes()
+	 */
 	@Override
 	protected Collection<Class<?>> getIdentifyingTypes() {
 		return Collections.singleton(ReactiveCouchbaseRepository.class);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.repository.config.RepositoryConfigurationExtensionSupport#postProcess(org.springframework.beans.factory.support.BeanDefinitionBuilder, org.springframework.data.repository.config.XmlRepositoryConfigurationSource)
+	 */
+	@Override
+	public void postProcess(BeanDefinitionBuilder builder, XmlRepositoryConfigurationSource config) {
+
+		Element element = config.getElement();
+		ParsingUtils.setPropertyReference(builder, element, REACTIVE_COUCHBASE_TEMPLATE_REF, "reactiveCouchbaseOperations");
+		ParsingUtils.setPropertyReference(builder, element, COUCHBASE_INDEX_MANAGER_REF, "indexManager");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.repository.config.RepositoryConfigurationExtensionSupport#postProcess(org.springframework.beans.factory.support.BeanDefinitionBuilder, org.springframework.data.repository.config.AnnotationRepositoryConfigurationSource)
+	 */
+	@Override
+	public void postProcess(BeanDefinitionBuilder builder, AnnotationRepositoryConfigurationSource config) {
+
+		builder.addDependsOn(BeanNames.REACTIVE_COUCHBASE_OPERATIONS_MAPPING);
+		builder.addDependsOn(BeanNames.COUCHBASE_INDEX_MANAGER);
+		builder.addPropertyReference("couchbaseOperationsMapping", BeanNames.REACTIVE_COUCHBASE_OPERATIONS_MAPPING);
+		builder.addPropertyReference("indexManager", BeanNames.COUCHBASE_INDEX_MANAGER);
+	}
+
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.repository.config.RepositoryConfigurationExtensionSupport#useRepositoryConfiguration(org.springframework.data.repository.core.RepositoryMetadata)
+	 */
 	protected boolean useRepositoryConfiguration(RepositoryMetadata metadata) {
 		return metadata.isReactiveRepository();
 	}
