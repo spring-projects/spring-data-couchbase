f4eee34faf0da94c332caff841df92a43797508c
Andrea Torlaschi <runnerway@gmail.com>
2020-06-09 01:19:04 +0200

    DATACOUCH-560 - Serialization of CouchbaseDocument and CouchbaseList.
    Add support for saving document with lists and composed objects.
    Convert CouchbaseDocument/CouchbaseList to Map/List before saving.

---
+++ src/test/java/org/springframework/data/couchbase/domain/Library.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2012-2020 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.couchbase.domain;
+
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.couchbase.core.mapping.Document;
+
+@Document
+public class Library {
+
+	@Id String id;
+	List<String> books;
+
+	public Library(String id, List<String> books) {
+		this.id = id;
+		this.books = books;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public List<String> getBooks() {
+		return books;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+
+		Library library = (Library) o;
+
+		if (id != null ? !id.equals(library.id) : library.id != null)
+			return false;
+		return books != null ? books.equals(library.books) : library.books == null;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = id != null ? id.hashCode() : 0;
+		result = 31 * result + (books != null ? books.hashCode() : 0);
+		return result;
+	}
+
+}
