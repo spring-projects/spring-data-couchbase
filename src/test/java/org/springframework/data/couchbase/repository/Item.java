e07e419c753ab3968b786146f93c406c0576e707
mikereiche <michael.reiche@couchbase.com>
2020-06-30 13:22:33 -0700

    DATACOUCH-484 - Thread Safety of query parameters.
    The test case  has been copied from 3.2.x for testing backport of fix in DATACOUCH-484
    That and dependent class have been copied from src/integration to src/test
    and the repository classes have been moved from org.springframework.data.couchbase.repository
    to org.springframework.data.couchbase.repo so that
    org.springframework.data.couchbase.repository.CouchbaseRepository is not instantiated by
    @Autowired

---
+++ src/test/java/org/springframework/data/couchbase/repository/Item.java
@@ -0,0 +1,38 @@
+package org.springframework.data.couchbase.repository;
+
+import com.couchbase.client.java.repository.annotation.Field;
+
+import org.springframework.data.annotation.Id;
+
+public class Item {
+
+  @Id
+  public String id;
+
+  @Field("desc")
+  public String description;
+
+  public Item(String id, String description) {
+    this.id = id;
+    this.description = description;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    Item item = (Item) o;
+
+    if (!id.equals(item.id)) return false;
+    return !(description != null ? !description.equals(item.description) : item.description != null);
+
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id.hashCode();
+    result = 31 * result + (description != null ? description.hashCode() : 0);
+    return result;
+  }
+}
