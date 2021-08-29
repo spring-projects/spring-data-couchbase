1089c727b95f06e2b3b0b20d89061da33aaa7a06
Charles <charleszkq@gmail.com>
2021-08-29 18:29:30 -0500

    fixed test can be flake out due to HashMap iteration order

--- src/test/java/org/springframework/data/couchbase/core/mapping/MappingCouchbaseConverterTests.java
+++ src/test/java/org/springframework/data/couchbase/core/mapping/MappingCouchbaseConverterTests.java
@@ -476,7 +476,12 @@ public class MappingCouchbaseConverterTests {
 
 		assertThat(valueStr).isEqualTo(((CouchbaseList) converted.getContent().get("listOfValues")).get(0));
 		assertThat(value2Str).isEqualTo(((CouchbaseList) converted.getContent().get("listOfValues")).get(1));
-		assertThat(converted.export().toString()).isEqualTo(source.export().toString());
+		assertThat(converted.getContent().get("listOfValues").toString()).isEqualTo(source.getContent().get("listOfValues").toString());
+		CouchbaseDocument sourceVals = (CouchbaseDocument) source.getContent().get("mapOfValues");
+		CouchbaseDocument convertedVals = (CouchbaseDocument) converted.getContent().get("mapOfValues");
+
+		assertThat(sourceVals.getContent().get("val1").toString()).isEqualTo(convertedVals.getContent().get("val1").toString());
+		assertThat(sourceVals.getContent().get("val2").toString()).isEqualTo(convertedVals.getContent().get("val2").toString());
 
 		CustomEntity readConverted = converter.read(CustomEntity.class, source);
 		assertThat(readConverted.value).isEqualTo(value);
