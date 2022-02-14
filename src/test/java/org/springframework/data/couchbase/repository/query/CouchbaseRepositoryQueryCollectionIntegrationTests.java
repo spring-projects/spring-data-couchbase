5c5dde3db132b73171b3d1836df41e68fd519395
Michael Reiche <48999328+mikereiche@users.noreply.github.com>
2022-02-14 09:09:09 -0800

    Collection support for predicates on meta fields. (#1338)
    When a derived query is being created and there is a predicate on
    a meta-field ( meta().id, cas, expiry), translate that field
    (i.e. id -> meta().id) *without* the bucket or collection name,
    as it might apply to a collelction which we do not know yet
    (for instance, if there is a withCollection() specified)..
    This is fine, because in a derived query, meta() without the
    bucket or collection name is unambigous.

--- src/test/java/org/springframework/data/couchbase/repository/query/CouchbaseRepositoryQueryCollectionIntegrationTests.java
+++ src/test/java/org/springframework/data/couchbase/repository/query/CouchbaseRepositoryQueryCollectionIntegrationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2021 the original author or authors.
+ * Copyright 2017-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -89,6 +89,16 @@ public class CouchbaseRepositoryQueryCollectionIntegrationTests extends Collecti
 		// no-op
 		// then call the super method
 		super.afterEach();
+	}
+
+	@Test
+	void findByKey() {
+		UserCol userCol = new UserCol("101", "userColFirst", "userColLast");
+		userColRepository.save(userCol);
+		UserCol found = userColRepository.getById(userCol.getId());
+		System.err.println("found: " + found);
+		assertEquals(userCol, found);
+		userColRepository.delete(found);
 	}
 
 	@Test
