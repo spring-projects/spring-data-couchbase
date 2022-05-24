7d539855383c19cf165b1c8b313c07cfbca3a14f
Graham Pople <grahampople@gmail.com>
2022-05-24 16:17:20 +0100

    Refactoring CouchbaseSimpleCallbackTransactionManager ThreadLocalStorage management

--- src/main/java/org/springframework/data/couchbase/transaction/CouchbaseSimpleCallbackTransactionManager.java
+++ src/main/java/org/springframework/data/couchbase/transaction/CouchbaseSimpleCallbackTransactionManager.java
@@ -16,32 +16,26 @@
 package org.springframework.data.couchbase.transaction;
 
 import com.couchbase.client.core.transaction.CoreTransactionAttemptContext;
-import com.couchbase.client.core.transaction.CoreTransactionGetResult;
-import com.couchbase.client.java.transactions.ReactiveTransactionAttemptContext;
 import com.couchbase.client.java.transactions.TransactionAttemptContext;
-import com.couchbase.client.java.transactions.TransactionGetResult;
+import com.couchbase.client.java.transactions.TransactionResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.couchbase.CouchbaseClientFactory;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionException;
 import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.reactive.TransactionContextManager;
 import org.springframework.transaction.support.CallbackPreferringPlatformTransactionManager;
 import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
-import reactor.core.publisher.Mono;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.util.concurrent.atomic.AtomicReference;
 
 // todo gp experimenting with simplest possible CallbackPreferringPlatformTransactionManager, extending PlatformTransactionManager
 // not AbstractPlatformTransactionManager
 public class CouchbaseSimpleCallbackTransactionManager implements CallbackPreferringPlatformTransactionManager {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(CouchbaseTransactionManager.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(CouchbaseSimpleCallbackTransactionManager.class);
 
 	private final CouchbaseClientFactory couchbaseClientFactory;
 
@@ -56,26 +50,7 @@ public class CouchbaseSimpleCallbackTransactionManager implements CallbackPrefer
 			couchbaseClientFactory.getCluster().transactions().run(ctx -> {
 				CouchbaseTransactionStatus status = new CouchbaseTransactionStatus(null, true, false, false, true, null, null);
 
-				// Setting ThreadLocal storage
-				TransactionSynchronizationManager.setActualTransactionActive(true);
-				TransactionSynchronizationManager.initSynchronization();
-				// Oddly, TransactionSynchronizationManager.clear() does not clear resources
-				try {
-					TransactionSynchronizationManager.unbindResource(CoreTransactionAttemptContext.class);
-				}
-				// todo gp must be a nicer way...
-				catch (IllegalStateException err) {}
-				// todo gpx if we need this of course needs to be exposed nicely
-				CoreTransactionAttemptContext internal;
-				try {
-					Field field = TransactionAttemptContext.class.getDeclaredField("internal");
-					field.setAccessible(true);
-					internal = (CoreTransactionAttemptContext) field.get(ctx);
-				}
-				catch (Throwable err) {
-					throw new RuntimeException(err);
-				}
-				TransactionSynchronizationManager.bindResource(CoreTransactionAttemptContext.class, internal);
+				populateTransactionSynchronizationManager(ctx);
 
 				try {
 					execResult.set(callback.doInTransaction(status));
@@ -90,18 +65,43 @@ public class CouchbaseSimpleCallbackTransactionManager implements CallbackPrefer
 			return execResult.get();
 	}
 
+	private void populateTransactionSynchronizationManager(TransactionAttemptContext ctx) {
+		// Setting ThreadLocal storage
+		TransactionSynchronizationManager.setActualTransactionActive(true);
+		TransactionSynchronizationManager.initSynchronization();
+		// Oddly, TransactionSynchronizationManager.clear() does not clear resources
+		try {
+			TransactionSynchronizationManager.unbindResource(CoreTransactionAttemptContext.class);
+		}
+		// todo gp must be a nicer way...
+		catch (IllegalStateException err) {}
+		// todo gpx if we need this of course needs to be exposed nicely - apparently unsupported in JDK17 anyway
+		CoreTransactionAttemptContext internal;
+		try {
+			Field field = TransactionAttemptContext.class.getDeclaredField("internal");
+			field.setAccessible(true);
+			internal = (CoreTransactionAttemptContext) field.get(ctx);
+		}
+		catch (Throwable err) {
+			throw new RuntimeException(err);
+		}
+		TransactionSynchronizationManager.bindResource(CoreTransactionAttemptContext.class, internal);
+	}
+
 	@Override
 	public TransactionStatus getTransaction(TransactionDefinition definition) throws TransactionException {
-		return null;
+		throw new IllegalStateException("unimplemented!");
 	}
 
 	@Override
 	public void commit(TransactionStatus status) throws TransactionException {
-		System.out.println("commit");
+		// todo gpx somewhat nervous that commit/rollback/getTransaction are all left empty but things seem to be working
+		// anyway... - what are these used for exactly?
+		throw new IllegalStateException("unimplemented!");
 	}
 
 	@Override
 	public void rollback(TransactionStatus status) throws TransactionException {
-		System.out.println("rollback");
+		throw new IllegalStateException("unimplemented!");
 	}
 }
