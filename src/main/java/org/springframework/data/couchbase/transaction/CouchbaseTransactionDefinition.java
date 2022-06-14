7de591a39e32264afd5d810c61c5979195fbd384
Graham Pople <grahampople@gmail.com>
2022-06-14 11:20:11 +0100

    Fixing some code warnings
    Mostly removing unused code

--- src/main/java/org/springframework/data/couchbase/transaction/CouchbaseTransactionDefinition.java
+++ src/main/java/org/springframework/data/couchbase/transaction/CouchbaseTransactionDefinition.java
@@ -4,15 +4,8 @@ import com.couchbase.client.java.transactions.ReactiveTransactionAttemptContext;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 public class CouchbaseTransactionDefinition extends DefaultTransactionDefinition {
-
-  ReactiveTransactionAttemptContext atr;
-
   public CouchbaseTransactionDefinition(){
     super();
     setIsolationLevel(ISOLATION_READ_COMMITTED);
-  }
-
-  public void setAttemptContextReactive(ReactiveTransactionAttemptContext atr){
-    this.atr = atr;
   }
 }
