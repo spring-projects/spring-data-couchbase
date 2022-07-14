4974e0075ff6ec90092f758fd310e3053ccea4ff
Michael Reiche <48999328+mikereiche@users.noreply.github.com>
2022-07-13 18:13:42 -0700

    Add javadoc for save operations.
    Closes #1495.

--- src/main/java/org/springframework/data/couchbase/core/ReactiveCouchbaseOperations.java
+++ src/main/java/org/springframework/data/couchbase/core/ReactiveCouchbaseOperations.java
@@ -52,9 +52,29 @@ public interface ReactiveCouchbaseOperations extends ReactiveFluentCouchbaseOper
 	 */
 	CouchbaseClientFactory getCouchbaseClientFactory();
 
+	/**
+	 * Save the entity to couchbase.<br>
+	 * If there is no version property on the entity class, and this is in a transaction, use insert. <br>
+	 * If there is no version property on the entity class, and this is not in a transaction, use upsert. <br>
+	 * If there is a version property on the entity class, and it is non-zero, then this is an existing document, use
+	 * replace.<br>
+	 * Otherwise, there is a version property for the entity, but it is zero or null, use insert. <br>
+	 *
+	 * @param entity the entity to save in couchbase
+	 * @param scopeAndCollection for use by repositories only. these are varargs for the scope and collection.
+	 * @param <T> the entity class
+	 * @return
+	 */
 	<T> Mono<T> save(T entity, String... scopeAndCollection);
 
-	<T> Mono<Long> count(Query query, Class<T> personClass);
+	/**
+	 * Returns the count of documents found by the query.
+	 * @param query
+	 * @param domainType
+	 * @param <T>
+	 * @return
+	 */
+	<T> Mono<Long> count(Query query, Class<T> domainType);
 
 	/**
 	 * @return the default consistency to use for queries
