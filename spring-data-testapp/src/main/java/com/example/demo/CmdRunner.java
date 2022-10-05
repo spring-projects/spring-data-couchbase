889375bf9cdceac46ad9ad94cf2f4a9de4cf6323
mikereiche <michael.reiche@couchbase.com>
2022-10-05 12:27:58 -0700

    Update spring-data-testapp for transactions.
    Closed #1576.

--- spring-data-testapp/src/main/java/com/example/demo/CmdRunner.java
+++ spring-data-testapp/src/main/java/com/example/demo/CmdRunner.java
@@ -15,12 +15,12 @@
  */
 package com.example.demo;
 
-import com.couchbase.client.java.transactions.TransactionResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.data.couchbase.CouchbaseClientFactory;
 import org.springframework.data.couchbase.core.CouchbaseTemplate;
+import org.springframework.data.couchbase.transaction.error.TransactionSystemUnambiguousException;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
 /**
  * Components of the type CommandLineRunner are called right after the application start up. So the method *run* is
@@ -31,52 +31,70 @@ import org.springframework.stereotype.Component;
 @Component
 public class CmdRunner implements CommandLineRunner {
 
-	@Autowired AirportRepository airportRepository;
 	@Autowired CouchbaseTemplate template;
-	@Autowired CouchbaseClientFactory couchbaseClientFactory;
-	@Autowired AirportService airportService;
+	@Autowired AirlineGatesService airlineGatesService;
 
 	@Override
-	public void run(String... strings) throws Exception {
+	public void run(String... strings) {
 
+		try { // remove leftovers from previous run
+			template.removeById(AirlineGates.class).one("1");
+		} catch (Exception e) {}
 		try {
-			template.removeById(Airport.class).one("1");
-		} catch (Exception e){}
+			template.removeById(AirlineGates.class).one("2");
+		} catch (Exception e) {}
+
+		AirlineGates airlineGates1 = new AirlineGates("1", "JFK", "American Airlines", Long.valueOf(200)); //1
+		AirlineGates airlineGates2 = new AirlineGates("2", "JFK", "Lufthansa", Long.valueOf(200));
+		AirlineGates saved1 = airlineGatesService.save(airlineGates1);
+		AirlineGates saved2 = airlineGatesService.save(airlineGates2);
+		AirlineGates found1 = airlineGatesService.findById(saved1.getId()); //2
+		AirlineGates found2 = airlineGatesService.findById(saved2.getId());
+		System.err.println("initialized airlines");
+		System.err.println("  found before transferGates: " + found1);
+		System.err.println("  found before transferGates: " + found2);
+		System.err.println("this transferGates attempt will succeed");
+		// move 50 gates from airline1 to airline2
+		int gatesToTransfer=50;
+		airlineGatesService.transferGates(airlineGates1.getId(), airlineGates2.getId(), gatesToTransfer, null); //3
+		found1 = airlineGatesService.findById(saved1.getId());
+		found2 = airlineGatesService.findById(saved2.getId());
+		System.err.println("  found after  transferGates: " + found1); //4
+		System.err.println("  found after  transferGates: " + found2);
+		Assert.isTrue(found1.getGates().equals(airlineGates1.getGates()-gatesToTransfer), "should have transferred");
+		Assert.isTrue(found2.getGates().equals(airlineGates1.getGates()+gatesToTransfer), "should have transferred");
+		System.err.println("this transferGates attempt will fail");
+		// attempt to move 44 gates from airline1 to airline2, but it fails.
 		try {
-			template.removeById(Airport.class).one("2");
-		} catch (Exception e){}
-
-		Airport new_1 = new Airport("1", "JFK", "jfk");
-		Airport saved_1 = airportRepository.save(new_1);
-		Airport found_1 = airportRepository.findById(saved_1.getId()).get();
-		System.out.println("found using repository by id: " + found_1);
-
-		Airport new_2 = new Airport("2", "LGA", "lga");
-		Airport saved_2 = airportRepository.save(new_2);
-		Airport found_2 = airportRepository.findById(saved_2.getId()).get();
-		System.out.println("found using repository by id: " + found_2);
-		Airport found_3 = airportRepository.findByIata("JFK");
-		System.out.println("founding using repository findByIata: "+found_3);
-
-
-		airportService.transferGates(new_1.getId(), new_2.getId(), 50);
-
-		System.out.println("found after transferGates: "+airportRepository.findById(saved_1.getId()).get());
-		System.out.println("found after transferGates: "+airportRepository.findById(saved_2.getId()).get());
+			// 5
+			airlineGatesService.transferGates(airlineGates1.getId(), airlineGates2.getId(), 44, new SimulateErrorException());
+		} catch (RuntimeException rte) {
+			if (!(rte instanceof TransactionSystemUnambiguousException) && rte != null
+					&& rte.getCause() instanceof SimulateErrorException) {
+				throw rte;
+			}
+			System.err.println("  got exception "+rte);
+		}
+		System.err.println("  found after  transferGates: " + airlineGatesService.findById(airlineGates1.getId()));
+		System.err.println("  found after  transferGates: " + airlineGatesService.findById(airlineGates2.getId()));
+		Assert.isTrue(found1.getGates().equals(airlineGates1.getGates()-gatesToTransfer), "should be same as previous");
+		Assert.isTrue(found2.getGates().equals(airlineGates1.getGates()+gatesToTransfer), "should be same as previous");
+		System.err.println("this transferGates attempt will succeed");
+		try {
+			// 5
+			airlineGatesService.transferGatesReactive(airlineGates1.getId(), airlineGates2.getId(), 44, null).block();
+		} catch (RuntimeException rte) {
+			if (!(rte instanceof TransactionSystemUnambiguousException) && rte != null
+					&& rte.getCause() instanceof SimulateErrorException) {
+				throw rte;
+			}
+			System.err.println("  got exception "+rte);
+		}
+		System.err.println("  found after  transferGates: " + airlineGatesService.findById(airlineGates1.getId()));
+		System.err.println("  found after  transferGates: " + airlineGatesService.findById(airlineGates2.getId()));
+		Assert.isTrue(found1.getGates().equals(airlineGates1.getGates()-gatesToTransfer), "should have transferred");
+		Assert.isTrue(found2.getGates().equals(airlineGates1.getGates()+gatesToTransfer), "should have transferred");
 	}
 
-
-	public void transferGatesDeprecated(String fromId, String toId, int gatesToTransfer) {
-		TransactionResult txResult = template.getCouchbaseClientFactory().getCluster().transactions().run(ctx -> {
-
-			Airport fromAirport = template.findById(Airport.class).one(fromId);
-			Airport toAirport = template.findById(Airport.class).one(toId);
-			toAirport.gates += gatesToTransfer;
-			fromAirport.gates -= gatesToTransfer;
-			template.save(fromAirport);
-			template.save(toAirport);
-		});
-
-	}
-
+	static class SimulateErrorException extends RuntimeException {}
 }
