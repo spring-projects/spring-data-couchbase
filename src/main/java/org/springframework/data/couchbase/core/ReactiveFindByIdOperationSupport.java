5b04d59571b98524489b921579df0a15148b1a6d
Michael Reiche <48999328+mikereiche@users.noreply.github.com>
2023-01-11 12:43:25 -0800

    Use getAndTouch only when annotation attribute touchOnRead=true. (#1641)
    Closes #1634.

--- src/main/java/org/springframework/data/couchbase/core/ReactiveFindByIdOperationSupport.java
+++ src/main/java/org/springframework/data/couchbase/core/ReactiveFindByIdOperationSupport.java
@@ -146,13 +146,12 @@ public class ReactiveFindByIdOperationSupport implements ReactiveFindByIdOperati
 			CommonOptions<?> getOptions;
 			final CouchbasePersistentEntity<?> entity = template.getConverter().getMappingContext()
 					.getRequiredPersistentEntity(domainType);
-			Duration entityExpiryAnnotation = entity.getExpiryDuration();
-			if (expiry != null || entityExpiryAnnotation == null || !entityExpiryAnnotation.isZero()
-					|| options instanceof GetAndTouchOptions) {
+			Boolean isTouchOnRead = entity.isTouchOnRead();
+			if (expiry != null || isTouchOnRead	|| options instanceof GetAndTouchOptions) {
 				if (expiry != null) {
 					expiryToUse = expiry;
-				} else if (entityExpiryAnnotation == null || !entityExpiryAnnotation.isZero()) {
-					expiryToUse = entityExpiryAnnotation;
+				} else if (isTouchOnRead) {
+					expiryToUse = entity.getExpiryDuration();
 				} else {
 					expiryToUse = Duration.ZERO;
 				}
@@ -173,7 +172,7 @@ public class ReactiveFindByIdOperationSupport implements ReactiveFindByIdOperati
 			}
 			return getOptions;
 		}
-		
+
 	}
 
 }
