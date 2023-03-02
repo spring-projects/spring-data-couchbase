b1ee7ca2a12fc42a92a20390fd6dbcc267436b78
Andy Toone <andy.toone@sky.uk>
2023-03-02 16:51:34 +0000

    ReactiveCouchbaseTemplate can overwrite PseudoArgs under heavy concurrency.
    Raised as issue #1684

--- src/main/java/org/springframework/data/couchbase/core/ReactiveCouchbaseTemplate.java
+++ src/main/java/org/springframework/data/couchbase/core/ReactiveCouchbaseTemplate.java
@@ -251,7 +251,14 @@ public class ReactiveCouchbaseTemplate implements ReactiveCouchbaseOperations, A
 	 * set the ThreadLocal field
 	 */
 	public void setPseudoArgs(PseudoArgs<?> threadLocalArgs) {
-		this.threadLocalArgs = new ThreadLocal<>();
+		if (this.threadLocalArgs == null) {
+			synchronized (this) {
+				if (this.threadLocalArgs == null) {
+					this.threadLocalArgs = new ThreadLocal<>();
+				}
+			}
+		}
+
 		this.threadLocalArgs.set(threadLocalArgs);
 	}
 
