afaf2ec97cee6b8657b77a0a9884edd1367afebd
Michael Reiche <48999328+mikereiche@users.noreply.github.com>
2023-10-11 15:38:39 -0700

    Don't use String version of enum parameters. (#1846)
    Just use the converted value as-is (in case of boolean or Number) enum value.
    
    Closes #1837.

--- src/test/java/org/springframework/data/couchbase/domain/UserRepository.java
+++ src/test/java/org/springframework/data/couchbase/domain/UserRepository.java
@@ -19,6 +19,7 @@ package org.springframework.data.couchbase.domain;
 import java.util.List;
 import java.util.stream.Stream;
 
+import com.fasterxml.jackson.annotation.JsonValue;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.couchbase.repository.Collection;
 import org.springframework.data.couchbase.repository.CouchbaseRepository;
@@ -60,6 +61,41 @@ public interface UserRepository extends CouchbaseRepository<User, String> {
 	List<User> getByFirstnameOrLastname(@Param("first") String firstname, @Param("last") String lastname);
 
 	List<User> findByIdIsNotNullAndFirstnameEquals(String firstname);
+
+	List<User> findByFirstname(@Param("firstName")FirstName firstName );
+
+	List<User> findByFirstnameIn(@Param("firstNames")FirstName[] firstNames );
+
+	@Query("#{#n1ql.selectEntity} where #{#n1ql.filter} and (firstname = $firstName)")
+	List<User> queryByFirstnameNamedParameter(@Param("firstName")FirstName firstName );
+
+	@Query("#{#n1ql.selectEntity} where #{#n1ql.filter} and (firstname = $1)")
+	List<User> queryByFirstnamePositionalParameter(@Param("firstName")FirstName firstName );
+
+	enum FirstName {
+		Dave,
+		William
+	}
+
+	@Query("#{#n1ql.selectEntity} where #{#n1ql.filter} and (jsonNode.myNumber = $myNumber)")
+	List<User> queryByIntegerEnumNamed(@Param("myNumber")IntEnum myNumber );
+
+	@Query("#{#n1ql.selectEntity} where #{#n1ql.filter} and (jsonNode.myNumber = $1)")
+	List<User> queryByIntegerEnumPositional(@Param("myNumber")IntEnum myNumber );
+
+	enum IntEnum {
+		One(1),
+		Two(2),
+		OneThousand(1000);
+		Integer value;
+    IntEnum(Integer i){
+			value = i;
+		}
+		@JsonValue
+		public Integer getValue(){
+			return value;
+		}
+	}
 
 	List<User> findByVersionEqualsAndFirstnameEquals(Long version, String firstname);
 
