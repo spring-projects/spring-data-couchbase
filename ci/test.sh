395c7aa8113178b95319a11be15635d5a113825c
Mark Paluch <mark.paluch@broadcom.com>
2024-10-09 08:57:23 +0200

    Fix CI permission issues.
    Consistently build all steps with the same docker user.
    
    Closes #1973

--- ci/test.sh
+++ ci/test.sh
@@ -3,10 +3,9 @@
 set -euo pipefail
 
 mkdir -p /tmp/jenkins-home/.m2/spring-data-couchbase
-chown -R 1001:1001 .
 
 export JENKINS_USER=${JENKINS_USER_NAME}
 
 MAVEN_OPTS="-Duser.name=${JENKINS_USER} -Duser.home=/tmp/jenkins-home" \
   ./mvnw -s settings.xml \
-  -P${PROFILE} clean dependency:list test -Dsort -U -B -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-data-couchbase
+  -P${PROFILE} clean dependency:list test -Dsort -U -B -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-data-couchbase -Ddevelocity.storage.directory=/tmp/jenkins-home/.develocity-root
